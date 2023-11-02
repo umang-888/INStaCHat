5540d2ef449059db3d57194f2d364f1386cfc3cf
raychiu99 <cchiu16@ucsc.edu>
2023-11-02 01:46:18 -0700

    initial chat page

--- mern-chat/client/src/Routes.jsx
+++ mern-chat/client/src/Routes.jsx
@@ -1,13 +1,13 @@
 import RegisterAndLogin from "./RegisterAndLogin.jsx"; 
 import { useContext } from "react";
 import { UserContext } from "./UserContext.jsx";
-
+import Chat from "./Chat.jsx"
 
 export default function Routes(){
     const {username, id} = useContext(UserContext);
 
     if (username){ 
-        return 'logged in!' + username;
+        return <Chat/>;
     }
 
     
